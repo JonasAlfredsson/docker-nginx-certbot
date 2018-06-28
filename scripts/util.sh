418560a7bed4ee188e5678e25b18e6e981ecdab0
Valder Gallo <valdergallo@gmail.com>
2018-06-27 22:19:19 -0300

    fix util sh

--- scripts/util.sh
+++ scripts/util.sh
@@ -63,15 +63,15 @@ get_certificate() {
     STAGING_URL='https://acme-staging.api.letsencrypt.org/directory'
 
     if [ "${IS_STAGING}" = "1" ]; then
-        letsencrypt_url=STAGING_URL
+        letsencrypt_url=$STAGING_URL
         echo "Staging ..."
-    elses
-        letsencrypt_url=PRODUCTION_URL
+    else
+        letsencrypt_url=$PRODUCTION_URL
         echo "Production ..."
     fi
 
-    echo "running certbot ... $letsencrypt_url"
+    echo "running certbot ... $letsencrypt_url $1 $2"
     certbot certonly --agree-tos --keep -n --text --email $2 --server \
         $letsencrypt_url -d $1 --http-01-port 1337 \
-        --standalone --standalone-supported-challenges http-01 --debug
+        --standalone --preferred-challenges http-01 --debug
 }
