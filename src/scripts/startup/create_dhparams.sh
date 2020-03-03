7db947f603e93c76a7efc719ca8498bf06f2d03e
jonas <jonas.alfredsson@protonmail.com>
2020-03-03 21:05:49 +0100

    Refactor dhparam startup script and add log message

--- src/scripts/startup/create_dhparams.sh
+++ src/scripts/startup/create_dhparams.sh
@@ -4,16 +4,18 @@
 . $(cd $(dirname $0); pwd)/../util.sh
 
 # Find any mentions of Diffie-Hellman parameters and create them if missing
-handle_dhparams() {
-    for file in $(parse_dhparams $1); do
-        if [ ! -f $file ]; then
-            echo "Couldn't find the dhparam file $file; creating it..."
-            create_dhparam $file
-            chmod 600 $file
+files_created=0
+for conf_file in /etc/nginx/conf.d/*.conf*; do
+    for dh_file in $(parse_dhparams $conf_file); do
+        if [ ! -f $dh_file ]; then
+            echo "Couldn't find the dhparam file $dh_file; creating it..."
+            create_dhparam $dh_file
+            chmod 600 $dh_file
+            files_created=$((files_created+1))
         fi
     done
-}
-
-for conf_file in /etc/nginx/conf.d/*.conf*; do
-    handle_dhparams $conf_file
 done
+
+if [ "$files_created" -eq "0" ]; then
+    echo "There was no need to create any dhparam files"
+fi
