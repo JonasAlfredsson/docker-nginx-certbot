90be9446e472fe7dd96ffc74a4647be77ba2d22f
jonas <jonas.alfredsson@protonmail.com>
2020-03-07 20:40:46 +0100

    Make `create_dhparams.sh` a stand-alone script
    Moved the functions used by the Diffie-Hellman parameter creation
    script from the `util.sh` file into the `create_dhparams.sh` file.
    This was done since these functions were only used by this script,
    and it makes more sense to have them all in the same location. This
    also made the `util` file a little bit less cluttered.

--- src/scripts/startup/create_dhparams.sh
+++ src/scripts/startup/create_dhparams.sh
@@ -1,7 +1,46 @@
 #!/bin/bash
 
-# Source "util.sh" so we can have our nice tools.
-. $(cd $(dirname $0); pwd)/../util.sh
+# This method may take an extremely long time to complete, be patient.
+# It should be possible to use the same dhparam file for all sites, just specify
+# the same file path under the "ssl_dhparam" parameter in the Nginx server
+# config. The file should be stored somewhere under /etc/letsencrypt/dhparams/
+# to ensure persistence between restarts.
+create_dhparam() {
+    if [ -z "$DHPARAM_SIZE" ]; then
+        echo "DHPARAM_SIZE unset, using default of 2048 bits"
+        DHPARAM_SIZE=2048
+    fi
+
+    echo "
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %                        ATTENTION!                       %
+    %                                                         %
+    % This script will now create a $DHPARAM_SIZE bit Diffie-Hellman   %
+    % parameter to use during the SSL handshake.              %
+    %                                                         %
+    % >>>>>      This MIGHT take a VERY long time!      <<<<< %
+    %    (Took 65 minutes for 4096 bit on an old 3GHz CPU)    %
+    %                                                         %
+    % However, there is some randomness involved so it might  %
+    % be both faster or slower for you. 2048 is secure enough %
+    % for today and quite fast to generate. These files will  %
+    % only have to be created once so please be patient.      %
+    % A message will be displayed when this process finishes. %
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    "
+    echo "Output file > $1"
+    openssl dhparam -out $1 $DHPARAM_SIZE
+    echo "
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % >>>>>   Diffie-Hellman parameter creation done!   <<<<< %
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    "
+}
+
+# Return all unique "dhparam" file paths.
+parse_dhparams() {
+    sed -n -r -e 's&^\s*ssl_dhparam\s+(.*);.*&\1&p' "$1" | xargs -n1 echo | uniq
+}
 
 # Find any mentions of Diffie-Hellman parameters and create them if missing.
 files_created=0
