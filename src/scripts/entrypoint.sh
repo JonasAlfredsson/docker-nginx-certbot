b1f8070254a809b9004258bea41d21a9317a663d
jonas <jonas.alfredsson@protonmail.com>
2020-12-13 10:19:25 +0100

    Make it possible to run 'run_certbot.sh' as a standalone script
    This change is made so that it should be possible to manually trigger
    a reload of certbot, without restarting the container.
    
    For this to be possible I had to move the "update loop" back to the
    entrypoint script, but I think it makes more sense to have it there
    after this change. Now it will be possible to run a "docker exec" on
    the 'run_certbot.sh' script to manually trigger a renewal of the
    certificates.

--- src/scripts/entrypoint.sh
+++ src/scripts/entrypoint.sh
@@ -38,9 +38,28 @@ echo "Starting the Nginx service"
 nginx -g "daemon off;" &
 NGINX_PID=$!
 
-# Start the certbot certificate management script.
-sleep 5 # Give Nginx a little time to start.
-$(cd $(dirname $0); pwd)/run_certbot.sh &
+# Make sure a renewal interval is set before continuing.
+if [ -z "$RENEWAL_INTERVAL" ]; then
+    echo "RENEWAL_INTERVAL unset, using default of '8d'"
+    RENEWAL_INTERVAL='8d'
+fi
+
+# Instead of trying to run 'cron' or something like that, just sleep and
+# call on certbot after the defined interval.
+(
+while [ true ]; do
+    # Check that all dhparam files exists.
+    $(cd $(dirname $0); pwd)/create_dhparams.sh
+
+    # Run certbot to check if any certificates needs renewal.
+    $(cd $(dirname $0); pwd)/run_certbot.sh
+
+    # Finally we sleep for the defined time interval before checking the
+    # certificates again.
+    echo "Certbot will now sleep..."
+    sleep "$RENEWAL_INTERVAL"
+done
+) &
 CERTBOT_LOOP_PID=$!
 
 # Nginx and the certbot update-loop process are now our children. As a parent
--- src/scripts/run_certbot.sh
+++ src/scripts/run_certbot.sh
@@ -3,57 +3,37 @@
 # Source in util.sh so we can have our nice tools.
 . $(cd $(dirname $0); pwd)/util.sh
 
-# Make sure a renewal interval is set before continuing.
-if [ -z "$RENEWAL_INTERVAL" ]; then
-    echo "RENEWAL_INTERVAL unset, using default of '8d'"
-    RENEWAL_INTERVAL='8d'
+# We require an email to be able to request a certificate.
+if [ -z "$CERTBOT_EMAIL" ]; then
+    error "CERTBOT_EMAIL environment variable undefined; certbot will do nothing!"
+    exit 1
 fi
 
-# Instead of trying to run 'cron' or something like that, just sleep and
-# call on certbot after the defined interval.
-while [ true ]; do
-    # We require an email to be able to request a certificate.
-    if [ -z "$CERTBOT_EMAIL" ]; then
-        error "CERTBOT_EMAIL environment variable undefined; certbot will do nothing!"
-        exit 1
-    fi
+echo "Starting certbot"
+# Go through all .conf files and find all domain names that should be added
+# to the certificate request.
+for conf_file in /etc/nginx/conf.d/*.conf*; do
+    for primary_domain in $(parse_primary_domains $conf_file); do
+        # At minimum we will make a request for the primary domain.
+        domain_request="-d $primary_domain"
 
-    # Check that all dhparam files exists.
-    $(cd $(dirname $0); pwd)/create_dhparams.sh
-
-    echo "Run certbot!"
-    # Go through all .conf files and find all domain names that should be added
-    # to the certificate request.
-    for conf_file in /etc/nginx/conf.d/*.conf*; do
-        for primary_domain in $(parse_primary_domains $conf_file); do
-            # At minimum we will make a request for the primary domain.
-            domain_request="-d $primary_domain"
-
-            # Find all 'server_names' in this .conf file and add them to the
-            # same request.
-            for server_name in $(parse_server_names $conf_file); do
-                domain_request="$domain_request -d $server_name"
-            done
-
-            # Hand over all the info required for the certificate request, and
-            # let certbot decide if it is necessary to update the certificate.
-            if ! get_certificate $primary_domain $CERTBOT_EMAIL "$domain_request"; then
-                error "Certbot failed for $primary_domain. Check the logs for details."
-            fi
+        # Find all 'server_names' in this .conf file and add them to the
+        # same request.
+        for server_name in $(parse_server_names $conf_file); do
+            domain_request="$domain_request -d $server_name"
         done
+
+        # Hand over all the info required for the certificate request, and
+        # let certbot decide if it is necessary to update the certificate.
+        if ! get_certificate $primary_domain $CERTBOT_EMAIL "$domain_request"; then
+            error "Certbot failed for $primary_domain. Check the logs for details."
+        fi
     done
-
-    # After trying to get all our certificates, auto enable any configs that we
-    # did indeed get certificates for.
-    auto_enable_configs
-
-    # Finally, tell Nginx to reload the configs.
-    nginx -s reload
-
-    # Finally we sleep for the defined time interval before checking the
-    # certificates again.
-    echo "Certbot will now sleep..."
-    sleep "$RENEWAL_INTERVAL"
 done
 
-exit 0
+# After trying to get all our certificates, auto enable any configs that we
+# did indeed get certificates for.
+auto_enable_configs
+
+# Finally, tell Nginx to reload the configs.
+nginx -s reload
