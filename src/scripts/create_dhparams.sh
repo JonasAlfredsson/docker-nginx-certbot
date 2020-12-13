d076a80390efc126a57b13e872ce7048dd354927
jonas <jonas.alfredsson@protonmail.com>
2020-12-13 09:52:14 +0100

    Remove unnecessary log output

--- src/scripts/create_dhparams.sh
+++ src/scripts/create_dhparams.sh
@@ -42,18 +42,12 @@ create_dhparam() {
 }
 
 # Find any mentions of Diffie-Hellman parameters and create them if missing.
-files_created=0
 for conf_file in /etc/nginx/conf.d/*.conf*; do
     for dh_file in $(parse_dhparams $conf_file); do
         if [ ! -f $dh_file ]; then
             echo "Couldn't find the dhparam file $dh_file; creating it..."
             create_dhparam $dh_file
             chmod 600 $dh_file
-            files_created=$((files_created+1))
         fi
     done
 done
-
-if [ "$files_created" -eq "0" ]; then
-    echo "There was no need to create any dhparam files"
-fi
