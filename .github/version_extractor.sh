438aabd92cbc8c90dd194b4325223e1f9c9810d7
jonas <jonas.alfredsson@protonmail.com>
2021-05-03 15:25:09 +0200

    Build more tags
    With this change we introduce the ability to lock the version on
    four different levels. The MAJOR tag will always point to the most
    recent MINOR, which in turn points to the most recent PATCH, and
    so on. This way users can determine how strict they want to be
    when downloading updates for this image.

--- .github/version_extractor.sh
+++ .github/version_extractor.sh
@@ -6,16 +6,23 @@ if [ -z "${1}" ]; then
     echo "Input argument was empty"
     exit 1
 fi
+echo "Received the following input argument: '${1}'"
 
-app_version=$(echo ${1} | sed -n -r -e 's&^refs/.+/v([1-9]\.[0-9]+\.[0-9]+).*$&\1&p')
+app_major=$(echo ${1} | sed -n -r -e 's&^refs/.+/v([1-9])\.[0-9]+\.[0-9]+.*$&\1&p')
+app_minor=$(echo ${1} | sed -n -r -e 's&^refs/.+/v[1-9]\.([0-9]+)\.[0-9]+.*$&\1&p')
+app_patch=$(echo ${1} | sed -n -r -e 's&^refs/.+/v[1-9]\.[0-9]+\.([0-9]+).*$&\1&p')
 nginx_version=$(echo ${1} | sed -n -r -e 's&^refs/.+/.*-nginx([1-9]\.[0-9]+\.[0-9]+)$&\1&p')
 
-if [ -n "${app_version}" -a -n "${nginx_version}" ]; then
-    echo "::set-output name=APP_VERSION::${app_version}"
+if [ -n "${app_major}" -a -n "${app_minor}" -a -n "${app_patch}" -a -n "${nginx_version}" ]; then
+    echo "::set-output name=APP_MAJOR::${app_major}"
+    echo "::set-output name=APP_MINOR::${app_minor}"
+    echo "::set-output name=APP_PATCH::${app_patch}"
     echo "::set-output name=NGINX_VERSION::${nginx_version}"
 else
     echo "Could not extract all expected values:"
-    echo "APP_VERSION=${app_version}"
+    echo "APP_MAJOR=${app_major}"
+    echo "APP_MINOR=${app_minor}"
+    echo "APP_PATCH=${app_patch}"
     echo "NGINX_VERSION=${nginx_version}"
     exit 1
 fi
