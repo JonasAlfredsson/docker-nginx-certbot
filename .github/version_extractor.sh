e824e4d28a7cca01ea78d6d45bc30e6d04d8ca02
jonas <jonas.alfredsson@protonmail.com>
2023-05-05 17:49:29 +0200

    Use 'GITHUB_ENV' instead of 'set-output'
    Resolves JonasAlfredsson/docker-nginx-certbot#162

--- .github/version_extractor.sh
+++ .github/version_extractor.sh
@@ -3,10 +3,9 @@
 # variable GITHUB_REF is passed in as the one and only argument.
 
 if [ -z "${1}" ]; then
-    echo "Input argument was empty"
+    >&2 echo "Input argument was empty"
     exit 1
 fi
-echo "Received the following input argument: '${1}'"
 
 app_major=$(echo ${1} | sed -n -r -e 's&^refs/.+/v([1-9])\.[0-9]+\.[0-9]+.*$&\1&p')
 app_minor=$(echo ${1} | sed -n -r -e 's&^refs/.+/v[1-9]\.([0-9]+)\.[0-9]+.*$&\1&p')
@@ -14,15 +13,12 @@ app_patch=$(echo ${1} | sed -n -r -e 's&^refs/.+/v[1-9]\.[0-9]+\.([0-9]+).*$&\1&
 nginx_version=$(echo ${1} | sed -n -r -e 's&^refs/.+/.*-nginx([1-9]\.[0-9]+\.[0-9]+)$&\1&p')
 
 if [ -n "${app_major}" -a -n "${app_minor}" -a -n "${app_patch}" -a -n "${nginx_version}" ]; then
-    echo "::set-output name=APP_MAJOR::${app_major}"
-    echo "::set-output name=APP_MINOR::${app_minor}"
-    echo "::set-output name=APP_PATCH::${app_patch}"
-    echo "::set-output name=NGINX_VERSION::${nginx_version}"
-else
-    echo "Could not extract all expected values:"
     echo "APP_MAJOR=${app_major}"
     echo "APP_MINOR=${app_minor}"
     echo "APP_PATCH=${app_patch}"
     echo "NGINX_VERSION=${nginx_version}"
+else
+    >&2 echo "Received the following input argument: '${1}'"
+    >&2 echo "Could not extract all expected values: v${app_major}.${app_minor}.${app_patch}-${nginx_version}"
     exit 1
 fi
